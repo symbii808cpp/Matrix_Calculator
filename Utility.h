671c18356c573fbce1ffb96b9d31cab499bebc88
symbii808cpp <mpilomotheo@gmail.com>
2025-06-11 16:13:15 +0200

    Deleted Utility.h

--- Utility.h
+++
@@ -1,9 +0,0 @@
-//Exception Classes
-class SquareMatrix_exception {};
-class EitherDimensions_exception {};
-class MutiplicationDimension_exception {};
-class ObjectSliced_exception {};
-
-class RescaleMatrix_exception {};
-class DoNothing_exception {};
-class ContinueExecution_exception {};
